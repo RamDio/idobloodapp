39e0b247c7c335cadaae59bb8e830f938a8b767d
RamDio <legendsclub418@gmail.com>
2020-03-21 16:14:47 +0800

    red cross url added

--- lib/home/events.dart
+++ lib/home/events.dart
@@ -1,18 +1,25 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:idobloodapp/drawer/maindrawer.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class Events extends StatelessWidget {
 
 
   @override
   Widget build(BuildContext context) {
+    final Completer<WebViewController>_controller=
+    Completer<WebViewController>();
+
+
     return Scaffold(
       backgroundColor: Colors.grey[100],
       appBar: AppBar(
         backgroundColor: Colors.red,
         title: Text(
-          "EVENTS",
+          "",
           style: TextStyle(color: Colors.black),
         ),
         elevation: 0.0,
@@ -20,6 +27,12 @@ class Events extends StatelessWidget {
       ),
 
         drawer:MainDrawer(),
+        body: WebView(
+          initialUrl: "https://redcross.org.ph/",
+          onWebViewCreated:(WebViewController webViewController){
+            _controller.complete(webViewController);
+          } ,
+        ),
     );
   }
 
