549b5ff7514a32f1e79e886b82187c0b2d8338c0
RamDio <legendsclub418@gmail.com>
2020-03-27 23:08:20 +0800

    insertuser.php

---
+++ lib/php script/insertuser.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'connection.php';
+// $userid =$_POST['userid'];
+$firstname =$_POST['firstname'];
+$lastname =$_POST['lastname'];
+$gender =$_POST['gender'];
+$contact =$_POST['contact'];
+$address =$_POST['address'];
+$bloodtype =$_POST['bloodtype'];
+$username =$_POST['username'];
+$password=$_POST['password'];
+
+$query="INSERT INTO tbluser(firstname,lastname,gender,contact,address,bloodtype,username,password)
+VALUES ('$firstname','$lastname','$gender','$contact','$address','$bloodtype','$username','$username','$password')";
+
+$exeQuery = mysqli_query($con,$query);
+
+if($exeQuery){
+    echo(json_encode(array('code'=> 1, 'message'=>'User Modified')));
+}else{
+    echo(json_encode(array('code'=> 2, 'message'=>'modification failed')));
+}
+
+
+
+?>
