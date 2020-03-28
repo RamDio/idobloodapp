7ba80f7ece791f2e99cfcd218b3353be6f33b672
RamDio <legendsclub418@gmail.com>
2020-03-28 22:05:23 +0800

    successful login entry

--- lib/login.dart
+++ lib/login.dart
@@ -25,7 +25,7 @@ class _LoginState extends State<Login> {
     Future<String> getLogin(String pseudo) async {
       var response = await http.get(
           Uri.encodeFull(
-              "https://192.168.18.7/idobloodapp/lib/php/login.php?PSEUDO=${pseudo}"),
+              "https://idobloodadmin.000webhostapp.com/login.php?PSEUDO=${pseudo}"),
           headers: {"Accept": "application/json"});
 
       print(response.body);
