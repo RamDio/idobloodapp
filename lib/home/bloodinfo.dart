d41e22ec13fb99b06411553c0edc2da0cf8e060b
RamDio <legendsclub418@gmail.com>
2020-05-09 23:00:40 +0800

    bloodinfopage edit 2

--- lib/home/bloodinfo.dart
+++ lib/home/bloodinfo.dart
@@ -4,7 +4,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'dart:convert';
-
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../drawer/maindrawer.dart';
 import 'updateprofile.dart';
 
@@ -25,117 +25,73 @@ class _BloodInfoState extends State<BloodInfo> {
           "Blood Type Info",
           style: TextStyle(color: Colors.black),
         ),
-
-       
       ),
       drawer: MainDrawer(),
       body: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-          
-          
             Padding(
-              padding: const EdgeInsets.only(left: 0.0),
+              padding: const EdgeInsets.only(left: 0.0,top:80.0),
               child: Row(
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.only(left: 48.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "RANK ",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 28.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          "Bronze ",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 28.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: SizedBox(
+                      height: 120.0,
+                      width: 120.0,
+                      child: Card(
+                        elevation: 10,
+                        child: Center(child: Text("A",style: TextStyle(
+                          fontSize:50.0
+                        ),)),
+                      ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 48.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "POINTS",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 28.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          "0 ",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 28.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
+                   Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: SizedBox(
+                      height: 120.0,
+                      width: 120.0,
+                      child: Card(
+                        elevation: 10,
+                        child: Center(child: Text("B",style: TextStyle(
+                          fontSize:50.0
+                        ),)),
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(left: 0.0),
+              padding: const EdgeInsets.only(left: 0.0,top:20.0),
               child: Row(
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(left: 48.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "RANK ",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 28.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          "Bronze ",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 28.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
+                   Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: SizedBox(
+                      height: 120.0,
+                      width: 120.0,
+                      child: Card(
+                        elevation: 10,
+                        child: Center(child: Text("O",style: TextStyle(
+                          fontSize:50.0
+                        ),)),
+                      ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 48.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text(
-                          "POINTS",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 28.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                        Text(
-                          "0 ",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 28.0,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ],
+                   Padding(
+                    padding: const EdgeInsets.only(left: 30.0),
+                    child: SizedBox(
+                      height: 120.0,
+                      width: 120.0,
+                      child: Card(
+                        elevation: 10,
+                        child: Center(child: Text("AB",style: TextStyle(
+                          fontSize:50.0
+                        ),)),
+                      ),
                     ),
                   ),
                 ],
