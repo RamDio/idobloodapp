8115f584a64c09cc34352c912f46a29d71d9ea3a
RamDio <legendsclub418@gmail.com>
2020-05-23 04:08:11 +0800

    blood page edited 1

--- lib/home/bloodinfo/a.dart
+++ lib/home/bloodinfo/a.dart
@@ -12,12 +12,55 @@ class A extends StatelessWidget {
          child:new Stack(
            children: <Widget>[
            new Container(
-             child: new Image.asset("name",
+             child: new Image.asset("asset/blhad.jpg",
               fit:BoxFit.cover,
               height: 300.0,
              ),
-            
-             )
+            constraints: new BoxConstraints.expand(height: 300.0),
+             ),
+
+             new Container(
+      margin: new EdgeInsets.only(top: 190.0),
+      height: 110.0,
+      decoration: new BoxDecoration(
+        gradient: new LinearGradient(
+          colors: <Color>[
+            new Color(0x00736AB7),
+            new Color(0xFF736AB7)
+          ],
+          stops: [0.0, 0.9],
+          begin: const FractionalOffset(0.0, 0.0),
+          end: const FractionalOffset(0.0, 1.0),
+        ),
+      ),
+    ),
+   
+      new ListView(
+        padding: new EdgeInsets.fromLTRB(0.0, 72.0, 0.0, 32.0),
+        children: <Widget>[
+        
+          new Container(
+            padding: new EdgeInsets.symmetric(horizontal: 32.0),
+            child: new Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top:148.0),
+                  child: Column(
+                    children: <Widget>[
+                      new Text("BLOOD", style: TextStyle(color:Colors.white,fontSize: 50,fontWeight: FontWeight.bold)),
+                       new Text( "", style: TextStyle())
+                    ],
+                  ),
+                ),
+       
+               
+              ],
+            ),
+          ),
+        ],
+
+    )
            ],)
        ),
      
