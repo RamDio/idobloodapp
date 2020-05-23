0821b48e2b3b454514dbd1e1089a18399c7a5c41
RamDio <legendsclub418@gmail.com>
2020-05-23 18:12:35 +0800

    blood page added description

--- lib/home/bloodinfo/a.dart
+++ lib/home/bloodinfo/a.dart
@@ -6,65 +6,62 @@ class A extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       body: new Container(
-         constraints:new BoxConstraints.expand(),
-         color: new Color(0xFF736AB7),
-         child:new Stack(
-           children: <Widget>[
-           new Container(
-             child: new Image.asset("asset/blhad.jpg",
-              fit:BoxFit.cover,
-              height: 300.0,
-             ),
-            constraints: new BoxConstraints.expand(height: 300.0),
-             ),
-
-             new Container(
-      margin: new EdgeInsets.only(top: 190.0),
-      height: 110.0,
-      decoration: new BoxDecoration(
-        gradient: new LinearGradient(
-          colors: <Color>[
-            new Color(0x00736AB7),
-            new Color(0xFF736AB7)
-          ],
-          stops: [0.0, 0.9],
-          begin: const FractionalOffset(0.0, 0.0),
-          end: const FractionalOffset(0.0, 1.0),
-        ),
-      ),
-    ),
-   
-      new ListView(
-        padding: new EdgeInsets.fromLTRB(0.0, 72.0, 0.0, 32.0),
-        children: <Widget>[
-        
-          new Container(
-            padding: new EdgeInsets.symmetric(horizontal: 32.0),
-            child: new Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(top:148.0),
-                  child: Column(
-                    children: <Widget>[
-                      new Text("BLOOD", style: TextStyle(color:Colors.white,fontSize: 50,fontWeight: FontWeight.bold)),
-                       new Text( "", style: TextStyle())
+      body: new Container(
+          constraints: new BoxConstraints.expand(),
+          color: new Color(0xFF736AB7),
+          child: new Stack(
+            children: <Widget>[
+              new Container(
+                child: new Image.asset(
+                  "asset/blhad.jpg",
+                  fit: BoxFit.cover,
+                  height: 300.0,
+                ),
+                constraints: new BoxConstraints.expand(height: 300.0),
+              ),
+              new Container(
+                margin: new EdgeInsets.only(top: 190.0),
+                height: 110.0,
+                decoration: new BoxDecoration(
+                  gradient: new LinearGradient(
+                    colors: <Color>[
+                      new Color(0x00736AB7),
+                      new Color(0xFF736AB7)
                     ],
+                    stops: [0.0, 0.9],
+                    begin: const FractionalOffset(0.0, 0.0),
+                    end: const FractionalOffset(0.0, 1.0),
                   ),
                 ),
-       
-               
-              ],
-            ),
-          ),
-        ],
-
-    )
-           ],)
-       ),
-     
-      
+              ),
+              new ListView(
+                padding: new EdgeInsets.fromLTRB(0.0, 72.0, 0.0, 32.0),
+                children: <Widget>[
+                  new Container(
+                    padding: new EdgeInsets.symmetric(horizontal: 32.0),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(top: 200.0),
+                          child: Column(
+                            children: <Widget>[
+                              new Text("BLOOD",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 50,
+                                      fontWeight: FontWeight.bold)),
+                              new Text("is a body fluid in humans and other animals that delivers necessary substances such as nutrients and oxygen to the cells and transports metabolic waste products away from those same cells. In vertebrates, it is composed of blood cells suspended in blood plasma ", style: TextStyle(fontSize: 20,color:Colors.white))
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
