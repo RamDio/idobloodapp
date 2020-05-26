c5f3aee6d20127a47cca152a825ff398b157ccf7
RamDio <legendsclub418@gmail.com>
2020-05-26 18:04:58 +0800

    bloodtype page dited

--- lib/home/bloodinfo/o.dart
+++ lib/home/bloodinfo/o.dart
@@ -14,12 +14,12 @@ class O extends StatelessWidget {
             children: <Widget>[
             
               new Container(
-                child: new Image.asset(
-                  "asset/bg.jpg",
-                  fit: BoxFit.cover,
-                  height: 300.0,
-                ),
-                constraints: new BoxConstraints.expand(height: 300.0),
+                // child: new Image.asset(
+                //   "asset/bg.jpg",
+                //   fit: BoxFit.cover,
+                //   height: 300.0,
+                // ),
+                // constraints: new BoxConstraints.expand(height: 300.0),
               ),
               new Container(
                 margin: new EdgeInsets.only(top: 190.0),
@@ -45,15 +45,22 @@ class O extends StatelessWidget {
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: <Widget>[
                         Padding(
-                          padding: const EdgeInsets.only(top: 200.0),
+                          padding: const EdgeInsets.only(top: 100.0),
                           child: Column(
                             children: <Widget>[
-                              new Text("BLOOD",
+                              new Text("Blood Types (Groups)",
                                   style: TextStyle(
                                       color: Colors.white,
                                       fontSize: 50,
                                       fontWeight: FontWeight.bold)),
-                              new Text("is a body fluid in humans and other animals that delivers necessary substances such as nutrients and oxygen to the cells and transports metabolic waste products away from those same cells. In vertebrates, it is composed of blood cells suspended in blood plasma ", style: TextStyle(fontSize: 20,color:Colors.white),),
+                              Padding(
+                                padding: const EdgeInsets.only(top:18.0),
+                                child: new Text(bloodtype, style: TextStyle(fontSize: 20,color:Colors.white),),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top:18.0),
+                                child: new Text(bloodtype1, style: TextStyle(fontSize: 20,color:Colors.white),),
+                              ),
                                
                             ],
                           ),
@@ -81,3 +88,26 @@ class O extends StatelessWidget {
             child: new BackButton(color: Colors.white),
           );
   }
+
+
+  String bloodtype="A blood type (also called a blood group) is defined"
+  " as the classification of blood based on the presence or absence of"
+  " inherited antigenic substances on the surface of red blood cells (RBCs). "
+  "A series of related blood types constitutes a blood group system, such as "
+  "the Rh or ABO system. The frequencies of the ABO and Rh blood types vary "
+  "from population to population.";
+
+  String bloodtype1="•	Blood Type A - If the red blood cell has only ""'A'"" molecules on it.\n"
+  "\n"
+"•	Blood Type B - If the red blood cell has only 'B' molecules on it.\n"
+  "\n"
+"•	Blood Type AB - If the red blood cell has a mixture of both" "A" "and" "B" "molecules.\n"
+  "\n"
+"•	Blood Type O - If the red blood cell has neither A or B molecule.\n"
+  "\n"
+
+"•	Receiving blood from the wrong ABO group can be life threatening. For example, if someone with group B blood is given group A blood, their anti-A antibodies will attack the group A cells.\n"
+  "\n"
+"•	This is why group A blood must never be given to someone who has group B blood and vice versa.\n"
+  "\n"
+"•	As group O red blood cells do not have any A or B antigens, it can safely be given to any other group.\n";
